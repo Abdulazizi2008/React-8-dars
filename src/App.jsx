7e0ae29944af2d4a09c1de7a6f07ddd73433492a
Abdulazizi2008 <ozodbekovabdu@gmail.com>
2024-07-10 23:36:57 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,83 @@
+import Header from "./components/Header";
+import SearchItem from "./components/SearchItem";
+import AddItem from "./components/AddItem";
+import Footer from "./components/Footer";
+import Content from "./components/Content";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState("true");
+  const [item, setItem] = useState("");
+  console.log(item);
+
+  const base_url = "http://localhost:3000";
+  useEffect(() => {
+    async function fetchItems() {
+      try {
+        const response = await fetch(`${base_url}/items`);
+        const data = await response.json();
+        setItems(data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    setTimeout(fetchItems, 2000);
+  }, []);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const newItem = {
+      id: items.length + 1,
+      item: item,
+      checked: false,
+    };
+    try {
+      const response = await fetch(`${base_url}/items`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newItem),
+      });
+      const data = await response.json();
+      setItems([...items, data]);
+      setItem("");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function handleDelete(id) {
+    try {
+      const response = await fetch(`${base_url}/items/${id}`, {
+        method: "DELETE",
+      });
+
+      const filteredItems = items.filter((item) => item.id !== id);
+      setItems(filteredItems);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return (
+    <div className="App">
+      <Header title="Grocery List" />
+      <AddItem setItem={setItem} item={item} handleSubmit={handleSubmit} />
+      <SearchItem />
+      <main>
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <Content items={items} handleDelete={handleDelete} />
+        )}
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
